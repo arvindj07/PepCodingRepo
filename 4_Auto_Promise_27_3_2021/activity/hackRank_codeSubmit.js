560996fbfc6a050f568618bbe86bffcc7f4ccbb6
arvindj07 <arvindj97@gmail.com>
2021-03-28 12:35:44 +0530

    add reference-file for links to study

--- 4_Auto_Promise_27_3_2021/activity/hackRank_codeSubmit.js
+++ 4_Auto_Promise_27_3_2021/activity/hackRank_codeSubmit.js
@@ -1,7 +1,7 @@
 let puppeteer = require('puppeteer');
 let {email,password}= require('../../secrets');
 
-let gtab;
+let gtab; // To store reference of the New-Tab 
 
 console.log("before");
 //browser launch
@@ -12,32 +12,35 @@ let browserWillBeLaunchedPromise= puppeteer.launch({
 });
 
 browserWillBeLaunchedPromise
-  .then(function (browserInstance){
+  .then(function (browserInstance){   // Launch the browser
     let newTabPromise = browserInstance.newPage();  // create new Tab
     return newTabPromise;
   })
-  .then(function (newTab){
+  .then(function (newTab){          // Redirected to New Tab/Page
     let loginPagePromise= newTab.goto(`https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login`);
 
     gtab=newTab;  // reference of the New-Tab formed
     return loginPagePromise;
   })
-  .then(function (){
-    let emailTypedPromise=gtab.type(`#input-1`, email, {delay: 100});
+  .then(function (){                // Go-to Login-Page
+    
+    let emailTypedPromise=gtab.type(`#input-1`, email, {delay: 100}); // type username
     return emailTypedPromise;
   })
   .then(function (){
-    let passwordTypedPromimse=gtab.type(`#input-2`, password, {delay: 100});
+    let passwordTypedPromimse=gtab.type(`#input-2`, password, {delay: 100}); // type password
     return passwordTypedPromimse;
   })
   .then(function (){
-    let loginPageClickPromise=gtab.click(`button[data-analytics="LoginPassword"]`);
+    let loginPageClickPromise=gtab.click(`button[data-analytics="LoginPassword"]`); // click on      
+                                                                                    //Submit-form
     return loginPageClickPromise;
   })
-  .then(function (){
-    console.log("Logged In")
+  .then(function (){    //  Logged-In and Reached DashBoard
+   
+    console.log("Logged-IN and at DashBoard");
   })
-  .catch(function (err){
+ .catch(function (err){
     console.log(err);
   })
 
--- 4_Auto_Promise_27_3_2021/raw/poc/puppeteer.js
+++ 4_Auto_Promise_27_3_2021/raw/poc/puppeteer.js
@@ -5,23 +5,42 @@ let browserWillBeLaunchedPromise= puppeteer.launch({
   headless:false ,  // to make the browser visible
 });
 
+//callback hell
+//-------------
+// browserWillBeLaunchedPromise
+//   .then(function (browserInstance){
+//     //new tab
+//     let newPagePromise= browserInstance.newPage();
+//     newPagePromise
+//       .then(function (newPage){
+//         console.log("New Tab Opened");
+//         // go to pepcoding-site from new tab
+//         let pageWillBeOpenedPromise=newPage.goto("https://pepcoding.com");
+//         pageWillBeOpenedPromise
+//           .then(function(){
+//             console.log("page is opened");
+//           })
+
+
+//       })
+
+//   })
+
+// Promise Chainning
 browserWillBeLaunchedPromise
   .then(function (browserInstance){
     //new tab
     let newPagePromise= browserInstance.newPage();
-    newPagePromise
-      .then(function (newPage){
-        console.log("New Tab Opened");
-        // go to pepcoding-site from new tab
-        let pageWillBeOpenedPromise=newPage.goto("https://pepcoding.com");
-        pageWillBeOpenedPromise
-          .then(function(){
-            console.log("page is opened");
-          })
-
-
-      })
-
+    return newPagePromise;
+  }).then(function (newPage){
+    console.log("New Tab Opened");
+    // go to pepcoding-site from new tab
+    let pageWillBeOpenedPromise=newPage.goto("https://pepcoding.com");
+    return pageWillBeOpenedPromise;
+  }).then(function(){
+    console.log("page is opened");
+  }).catch(function (err){
+    console.log(err);
   })
 
 console.log(browserWillBeLaunchedPromise);
