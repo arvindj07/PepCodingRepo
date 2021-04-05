aa087ea9efc76ba8b8dd08e5a5ed06af8fb4b3ce
arvindj07 <arvindj97@gmail.com>
2021-04-05 19:11:41 +0530

    working of then() func

--- 4_Auto_Promise_27_3_2021/raw/facts/promise/cb.js
+++ 4_Auto_Promise_27_3_2021/raw/facts/promise/cb.js
@@ -6,10 +6,15 @@ function chargeCreditCard() {
     amount = amount - tocut;
     console.log(`Remaining amount ${amount}`)
 }
+
+// function fn1() {
+//     amount = 0;
+//     console.log(`Remaining amount ${amount}`)
+// }
 console.log("Before");
 // async function
 // lib.analyzedata("TV", chargeCreditCard);
 let promiseObj = lib.promisifiedAnalyzedata("TV");
 promiseObj.then(chargeCreditCard);
-// promiseObj.then(chargeCreditCard);
+// promiseObj.then(fn1);    // this way,u can change the value of amount again
 console.log("After");
