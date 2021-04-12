7ec9959e50803eb94f20638b70d08d43a38e2701
arvindj07 <arvindj97@gmail.com>
2021-04-12 16:12:10 +0530

    Updated

--- 5_Auto_Asyn_Await_10_4_2021/activity/youtubePlayListAnalizer.js
+++ 5_Auto_Asyn_Await_10_4_2021/activity/youtubePlayListAnalizer.js
@@ -17,29 +17,34 @@ let fs = require("fs");
     // selector for no. of Videos nd Views
     let videoNdViewSelector = `#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer`;
     await page.waitForSelector(videoNdViewSelector);
-    let videosViewsArr = await page.evaluate(getVideosViews, videoNdViewSelector);
+    let videosViewsArr = await page.evaluate(getVideosViews, videoNdViewSelector);  // handler func
 
     let videos = videosViewsArr[0];
     let views = videosViewsArr[1];
     console.log("videos->" + videos);
     console.log("views->" + views);
 
-    let videosCount = videos.split(" ")[0]; // to get only count
-    videosCount = Number(videosCount);
+    // to convert video-Count from string to Number format 
+    let videosCount = videos.split(" ")[0];
+    videosCount = Number(videosCount);  // total no. of videos in Playlist
 
-    let durationSelector =`span.style-scope.ytd-thumbnail-overlay-time-status-renderer`;
+    // Selectors for Duration nd Title of each video
+    let durationSelector = `span.style-scope.ytd-thumbnail-overlay-time-status-renderer`;
     await page.waitForSelector(durationSelector);
     let titleSelector = `a#video-title`
     await page.waitForSelector(titleSelector);
 
+    // get total no. of videos loaded , after scrolling till bottom of page
     let pCurrentVideoCount = await scrollToBottom(page, titleSelector);
 
-    while (videosCount - 50 > pCurrentVideoCount) {
+    // scroll down the page , till the no. of loaded videos is approx equal to total no. of videos in playlist
+    while (videosCount - 50 > pCurrentVideoCount) { // -50 ka hum jugad lga rhe h, to terminate the loop
       pCurrentVideoCount = await scrollToBottom(page, titleSelector);
     }
 
-    let  timeDurArr=await page.evaluate(getStats, durationSelector, titleSelector);
-    console.table(timeDurArr);
+    // Get the Title nd Duration of all Videos in Playlist after every video gets loaded, after scrolling down till bottom
+    let timeDurArr = await page.evaluate(getStats, durationSelector, titleSelector);
+    console.table(timeDurArr);  
 
   } catch (err) {
     console.log("error-> " + err);
@@ -78,20 +83,15 @@ function getStats(durationSelector, titleSelector) {
 }
 
 // task-> to scroll till bottom of a Page, and return total no. of videos/titles loaded
-async function scrollToBottom(page,title){
+async function scrollToBottom(page, title) {
 
   // scroll-to-bottom nd return total no. of videos/titles loaded (Done inside DOM)
-  function getLengthConsoleFn(title){
-    window.scrollBy(0,window.innerHeight);
-    let titleElemArr=document.querySelectorAll(title);
-    // console.log("titleLength "+titleElemArr.length);
-
-    // if(titleElemArr.length==899){
-    //   console.log(titleElemArr);
-    // }
-
-    return titleElemArr.length; // return the whole length
+  function getLengthConsoleFn(title) {
+    window.scrollBy(0, window.innerHeight); // scroll till bottom
+    let titleElemArr = document.querySelectorAll(title);  // select all video-titles loaded
+    
+    return titleElemArr.length; // return the length, ie, total no. of videos loaded
   }
-  
-  return page.evaluate(getLengthConsoleFn,title);
+
+  return page.evaluate(getLengthConsoleFn, title);
 }
