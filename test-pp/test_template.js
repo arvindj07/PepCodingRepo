220821329525d7613b169b7ed0e20aa419cb9c61
arvindj07 <arvindj97@gmail.com>
2021-04-15 22:06:14 +0530

    test

---
+++ test-pp/test_template.js
@@ -0,0 +1,46 @@
+// q6
+// console.log(1);
+// setTimeout(function(){
+//   console.log(2)
+// });
+
+// console.log(3);
+// console.log(1);
+// setTimeout(function(){
+//   console.log(2)
+// });
+// console.log(1);
+// Promise.resolve().then(function(){
+//   console.log(5);
+// })
+// console.log(1);
+
+// q8
+// try {
+//   let a = null;
+
+//   b = a;
+
+//   delete a;
+//   b=undefined;
+
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// } catch (err) {
+//   console.log(err.message);
+// }
+
+function f(num1 , num2){
+  if(num1%2==0 && num1%num2==0){
+    return num1%num2;
+  }else{
+    throw new Error('incompatible types');
+  }
+}
+
+try{
+  console.log(f(4,3));
+}catch(err){
+  console.log(err.message)
+}
