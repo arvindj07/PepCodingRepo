ed805245172a7d1dca803df6bc958fe672c764e1
arvindj07 <arvindj97@gmail.com>
2021-04-26 01:39:29 +0530

    Todo App- Task2 Complete

--- 6_Browser_Todo_23_4_2021/activity/script.js
+++ 6_Browser_Todo_23_4_2021/activity/script.js
@@ -1,10 +1,23 @@
-let colorBtn=document.querySelectorAll(".filter_color");
-let mainContainer=document.querySelector(".main_container");
+let mainContainer = document.querySelector(".main_container");
+let modalContainer= document.querySelector(".modal-container");
+let colorBtn = document.querySelectorAll(".filter_color"); //Navbar-> Color Buttons
+let addTask = document.querySelector(".task-add"); // '+' button in Navbar
 
-for(let i=0;i<colorBtn.length;i++){
-  colorBtn[i].addEventListener('click',function(e){
-    let color=colorBtn[i].classList[1];// get color class at idx-1 in class-List
+// Add event-listener to Color-Button in Navbar
+for (let i = 0; i < colorBtn.length; i++) {
+  colorBtn[i].addEventListener('click', function (e) {
+    let color = colorBtn[i].classList[1];// get color class at idx-1 in class-List
 
-    mainContainer.style.backgroundColor=color; // set color-attribute of main-container
+    mainContainer.style.backgroundColor = color; // set color-attribute of main-container
   });
 }
+
+// Modal-Container appears on click on-> '+' button
+addTask.addEventListener('click',function(){
+  modalContainer.style.opacity=1;
+})
+
+// remember to reset modal-Container to empty and back to default selections
+
+
+
