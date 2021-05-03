44561f403bd99ce9eceb9ce2563e64102693c759
arvindj07 <arvindj97@gmail.com>
2021-05-03 21:47:50 +0530

    New Activity Excel Clone

--- 7_Browser_Excel_Clone_1_5_2021/activity/script.js
+++ 7_Browser_Excel_Clone_1_5_2021/activity/script.js
@@ -1,5 +1,13 @@
 let plusBtn = document.querySelector(".plusBtn_container");
 let sheetList = document.querySelector(".sheets_list");
+let allCells = document.querySelectorAll(".grid .col");
+let addressBox = document.querySelector(".address_box");
+// Align-Buttons
+let leftBtn = document.querySelector(".left");
+let rightBtn = document.querySelector(".right");
+let centerBtn = document.querySelector(".center");
+// font-size
+let fontBtn= document.querySelector(".font_size");
 
 // Add eventListener on first Sheet
 let firstSheet = document.querySelector(".sheet");
@@ -21,6 +29,7 @@ function handleActiveSheet(e) {
 
 }
 
+// Add eventListener to plus-button to add Sheets
 plusBtn.addEventListener("click", function (e) {
   // Get Idx of Last Sheet
   let sheetsArr = document.querySelectorAll(".sheet");
@@ -38,4 +47,68 @@ plusBtn.addEventListener("click", function (e) {
   // Add eventListener on new Sheet
   newSheet.addEventListener("click", handleActiveSheet);
 });
+
+// Add eventListener to all-cells
+for (let i = 0; i < allCells.length; i++) {
+  allCells[i].addEventListener("click", handleCell);
+}
+
+// 1st cell is clicked by-default
+allCells[0].click();
+
+// Set address of cell in Address-Box, when a cell is clicked
+function handleCell(e) {
+  currCell = e.currentTarget;
+  let rid = Number(currCell.getAttribute("rid"));
+  let cid = Number(currCell.getAttribute("cid"));
+  let rowAddress = rid + 1;
+  let colAddress = String.fromCharCode(cid + 65);
+  let address = colAddress + rowAddress;
+  addressBox.value = address;
+}
+
+
+// Add eventListner Alignment-Btn- i.e, L,C,R
+leftBtn.addEventListener("click", alignCell);
+rightBtn.addEventListener("click", alignCell);
+centerBtn.addEventListener("click", alignCell);
+
+// To set Text-align of clicked cell
+function alignCell(e) {
+  let alignBtn = e.currentTarget;
+  // address of clicked-cell
+  let address = addressBox.value;
+  let { rid, cid } = getRidCidfrommAddress(address);
+  let cell = document.querySelector(`div[rid="${rid}"][cid="${cid}"]`);
+  let alignDirec = alignBtn.getAttribute("class");
+  cell.style.textAlign = alignDirec;
+
+}
+
+// function to get rid and cid of cell, from address in address-box
+function getRidCidfrommAddress(address) {
+  // convert char to Ascii code  
+  let colAddress = address.charCodeAt(0);
+  let rowAddress = address.slice(1);
+
+  let cid = colAddress - 65;
+  let rid = Number(rowAddress) - 1;
+
+  return {cid,rid};
+}
+
+// Set Font-size, on change->event
+fontBtn.addEventListener("change",setFontSize);
+
+function setFontSize(e){
+  let fontSelectBtn=e.currentTarget;
+  let fontSize=fontSelectBtn.value;
+  // address of clicked-cell
+  let address = addressBox.value;
+  let { rid, cid } = getRidCidfrommAddress(address);
+  let cell = document.querySelector(`div[rid="${rid}"][cid="${cid}"]`);
+  cell.style.fontSize=fontSize+"px";
+
+}
+
 
