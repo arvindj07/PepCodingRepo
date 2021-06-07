1c4c1c0f002af9de5537490ff0f8eff702c3b26a
arvindj07 <arvindj97@gmail.com>
2021-06-07 23:46:56 +0530

    this,bind and arrow func in ES6

--- 9_React_ES6_5_6_2021/poc/Destructuring/Arrays.js
+++ 9_React_ES6_5_6_2021/poc/Destructuring/Arrays.js
@@ -1,34 +1,35 @@
-let introduction =['Hello','I','am','Tushar'];
+let introduction= ['Hello','I','am','Tushar'];
 // let greeting = introduction[0];
 // let pronoun = introduction[1];
 // let variable = introduction[2];
-// let name = introduction[3];
-
+// console.log(greeting+" "+pronoun+" "+variable);
+//////////////////////////////////////////Destructuring
 
 // let [greeting,pronoun,variable,name] = introduction;
-// console.log(greeting);
-// console.log(pronoun);
-// console.log(variable);
-// console.log(name);
+// console.log(greeting+" "+pronoun+" "+variable+" "+name);
+// greeting='Hola';
+// console.log(introduction);
 
-//skipping values
+////////////////////////skipping values
 
-// let [greeting,pronoun,,name]=introduction;
-// console.log(name);
-// console.log(greeting);
-// console.log(pronoun);
+let [greeting,pronoun,,name]=introduction;
+console.log(greeting+" "+pronoun+" "+name); 
 
-// let newarr =['Hello'];
-// let [greeting='Howdy',pronoun='has',name='Stark']=newarr;
-// console.log(greeting);
-// console.log(pronoun)
-// console.log(name);;
+/////////////////////////////////Default values
 
-// let a =3;
-// let b =6;
-// [a,b]=[b,a];
-// console.log(a);
-// console.log(b);
+let arr =['Howdy'];
+let [ab='hello',cd='bye',ef='say']=arr;
+console.log(ab+" "+cd+" "+ef);
+
+//////////////////////////////////swapping values
+
+let a =3;
+let b=6;
+[a,b] =[b,a];
+console.log(a);
+console.log(b);
+
+/////////////////////////// spread operator
 
 let arr = [1,2,3,4,5,6,7,8,9,10,11];
 let [fv,sv,tv,...narr]=arr;
--- 9_React_ES6_5_6_2021/poc/Destructuring/Objects.js
+++ 9_React_ES6_5_6_2021/poc/Destructuring/Objects.js
@@ -1,29 +1,34 @@
-let person = {name:'Steve',country:'Los Angeles',job:'Avenger'};
+let person = {
+  name:'Steve',
+  country:'Los Angeles',
+  job:'Avenger'
+}
 
 // let name = person.name;
-// let country = person.country;
+// let country = person['country'];
 // let job = person.job;
+
 // console.log(name);
 // console.log(country);
 // console.log(job);
+///////////////////////////////////Destructuring
 
-/////////Destructuring
-
-// let {name,country,job} =person;
+// let {name,country,job}=person;
 // console.log(name);
 // console.log(country);
 // console.log(job);
-/////////////////////////////////
-let {name,country,profession='default-value',job}=person;
+//////////////////////////////////////Undefined && default values
+
+
+var {name,country,job='jobs',anything='default value'}=person;
 console.log(name);
 console.log(country);
-console.log(profession);
 console.log(job);
+console.log(anything);
 
-//////////////////Aliases///////////////////
+//////////////////////////////////////////Alias
 
 // let {name:a,country:b,job:c}=person;
 // console.log(a);
 // console.log(b);
 // console.log(c);
-// console.log(person);
